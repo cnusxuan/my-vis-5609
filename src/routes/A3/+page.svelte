6c1bd858e4c7f2fc09c29389af817b0270ec5953
ehrma055 <ehrma055@umn.edu>
2026-03-25 22:59:02 -0500

    Fix prerender config for A3

--- src/routes/A3/+page.svelte
+++ src/routes/A3/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-    export const prerender = true;
     import { onMount } from "svelte";
     import * as THREE from "three";
     import { FontLoader, Font } from "three/addons/loaders/FontLoader.js";
